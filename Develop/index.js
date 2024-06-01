d03b871f30e7ec0f58688daf87ba27a3d275515b
Osorio-333 <osoriooscar619@gmail.com>
2024-06-01 12:45:39 -0700

    corrected mistakes

--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,81 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-// TODO: Create an array of questions for user input
-function init() {
-    inquirer
-        .prompt([
-            {
-                name: 'Title',
-                type: 'input',
-                message: 'Please Enter a Title',
-            },
-            {
-                name: 'Description',
-                type: 'input',
-                message: 'Please Enter a Description including Installation, Table of Contents, etc.',
-            },
-            {
-                name: 'License',
-                type: 'checkbox',
-                message: 'Please Choose a License',
-                choices: [
-                    "MIT License",
-                    "GNU GPLv3",
-                    "Apache License 2.0",
-                ]
-            },
-            {
-                name: 'GitHubUser',
-                type: 'input',
-                message: 'Please Enter your GitHub Username',
-            },
-            {
-                name: 'Email',
-                type: 'input',
-                message: 'Please Enter your email address.',
-            },
-        ])
-        .then((answers) => {
-            writeToFile('README.md', generateREADME(answers));
-        });
-}
-    
-    
-    function generateREADME(data) {
-        return `
-        # ${data.Title}
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-        ## Description
-        ${data.Description}
-
-        ## License
-        ${data.License.join(", ")}
-
-        ## Contact
-        GitHub: [${data.GitHubUser}](https://github.com/${data.GitHubUser})  
-        Email: ${data.Email}
-        `;
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "What command should be run for testing?",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE",
+            "GPL",
+            "BSD",
+            "None"
+        ]
     }
-// TODO: Create a function to write README file
+];
+
+// function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log('README.md generated successfully')
-    );
+    let content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('success')
+    });
 }
 
-// TODO: Create a function to initialize app
-// function init() {}
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        var fileName = 'README.md';
+        writeToFile(fileName, data)
+    });
+}
 
-// Function call to initialize app
+// function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,85 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === 'MIT License') {
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else if (license === 'GNU GPLv3') {
-      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  } else if (license === 'Apache License 2.0') {
-      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } else {
-      return '';
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'MIT License') {
-      return `[MIT License](https://opensource.org/licenses/MIT)`;
-  } else if (license === 'GNU GPLv3') {
-      return `[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)`;
-  } else if (license === 'Apache License 2.0') {
-      return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
-  } else {
-      return '';
-  }
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'MIT License') {
-      return `This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).`;
-  } else if (license === 'GNU GPLv3') {
-      return `This project is licensed under the [GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0).`;
-  } else if (license === 'Apache License 2.0') {
-      return `This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).`;
-  } else {
-      return '';
-  }
-}
-
-renderLicenseBadge();
-renderLicenseLink();
-renderLicenseSection();
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-# ${data.title}
-
-${renderLicenseBadge(data.license)}
-
-## Description
-${data.description}
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-${data.installation}
-
-## Usage
-${data.usage}
-
-${renderLicenseSection(data.license)}
-
-## Contributing
-${data.contributing}
-
-## Tests
-${data.tests}
-
-## Questions
-For questions about the project, please feel free to contact me via GitHub: [${data.github}](https://github.com/${data.github}) or via email at ${data.email}.
-`;
-}
-
-module.exports = generateMarkdown;
-
+    return `# ${data.title}
+    ---
+  
+    ![${data.license}](https://img.shields.io/badge/license-${data.license}-green)
+  
+    ## Description
+    ${data.description}
+  
+    ## Table of Contents
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Tests](#tests)
+    6. [Questions](#questions)
+  
+    ## Installation
+    To install the necessary dependencies, run the following command:
+    ${data.installation}
+  
+    ## Usage
+    ${data.usage}
+  
+    ## License 
+    This project is licensed under the ${data.license} license.
+  
+    ## Contributing
+    ${data.contribution}
+  
+    ## Tests
+    To run tests, run the following command:
+    ${data.tests}
+  
+    ## Questions
+    If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
+  `;
+  }
+  
+  module.exports = generateMarkdown;
